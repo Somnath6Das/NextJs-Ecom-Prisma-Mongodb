99121cc523dc94067717651a9e955214c6f7ad42
Somnath6das <somnath8das@gmail.com>
2024-06-18 20:21:01 +0530

    update

--- src/app/admin/loading.tsx
+++ src/app/admin/loading.tsx
@@ -2,7 +2,7 @@ import { Loader2 } from "lucide-react";
 
 export default function AdminLoading() {
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center align-center">
       <Loader2 className="size-24 animate-spin" />
     </div>
   );
